58bd38dddfde0ab7cc9ca444db75aa7b993c16f0
ZiCheng-Web <523186180@qq.com>
2020-02-29 03:13:08 +0800

    redis sentinel做缓存入门

--- src/main/java/com/zicheng/SpringbootRedisCacheApplication.java
+++ src/main/java/com/zicheng/SpringbootRedisCacheApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootRedisCacheApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootRedisCacheApplication.class, args);
     }
 
